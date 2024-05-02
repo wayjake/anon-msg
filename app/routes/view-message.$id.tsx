81509c60cc7d717b341579e4a7f1aa17e5c3ad58
Jacob Berg <jake@dubsado.com>
2024-05-02 16:37:08 -0600

    style cleanup and hydrated metadata

--- app/routes/view-message.$id.tsx
+++ app/routes/view-message.$id.tsx
@@ -3,10 +3,10 @@ import './styles.css'
 import { db } from "~/db.server";
 import { useLoaderData } from "@remix-run/react";
 
-export const meta: MetaFunction = ({ params }) => {
+export const meta: MetaFunction = ({ params, data }) => {
     return [
-        { title: `Message:${params.id}` },
-        { name: "description", content: "Here's the first 140 characters of this message!" },
+        { title: `@${data?.message?.username || 'Anon'} says... msg_id:${params.id}` },
+        { name: "description", content: `${data?.message?.content}` },
     ];
 };
 
@@ -18,33 +18,17 @@ export async function loader({ params }: ActionFunctionArgs) {
     return json({ message: message[0] })
 }
 
-{/* <pre>{JSON.stringify(message, undefined, 4)}</pre> */ }
-
-
-/** export default function Index() {
-    const { message } = useLoaderData()
-    return (
-
-        <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-            <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-80">
-                <div className="bg-gradient-to-r bg-pink-400 ">@{message.username || "Anon"} says</div>
-                <div className="bg-white text-gray-800"> {message.content}</div>
-            </div>
-        </div>
-    );
-}**/
-
 export default function Index() {
     const { message } = useLoaderData();
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100 text-white">
-            <div className="bg-gray-800 shadow-lg w-full max-w-md m-0 lg:max-w-sm rounded-lg">
+            <div className="bg-gray-800 shadow-lg w-4/12 max-w-sm m-0 lg:max-w-sm rounded-lg text-center">
                 {/* Gradient header with dynamic text */}
-                <div className="w-full p-6 leading-none bg-gradient-to-r from-pink-500 to-orange-400 text-white text-lg font-bold uppercase rounded-t-lg">
-                    <span></span>@{message.username || "Anon"} says
+                <div className="w-full p-6 leading-none bg-gradient-to-r from-pink-500 to-orange-400 text-white text-lg font-bold rounded-t-lg">
+                    <span className="underline">@{message.username || "Anon"}</span> says:
                 </div>
                 {/* White section with dynamic message content */}
-                <div className="bg-white text-gray-800 w-full p-6 leading-none rounded-b-lg">
+                <div className="bg-white text-gray-800 w-full p-6 leading-none font-bold rounded-b-lg">
                     {message.content}
                 </div>
             </div>
